dc06312b3e69a4535426c0eae6ef8f551214483f
jose.mallqui@efaps.org <jose.mallqui@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-10-18 22:07:23 +0000

    - data: create configurations files on package util
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/data/trunk@10592 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/data/util/DataSettings.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.data.util;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("e54e51c4-cd72-4846-8e73-33c0a540b3e7")
+@EFapsRevision("$Rev$")
+public interface DataSettings
+{
+}
