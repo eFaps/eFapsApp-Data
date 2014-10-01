84589c77dfb39f1e18337b5d435cdffb414b363f
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-10-01 15:40:10 +0000

    - data: adding datamodel and UserInterface for import
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/data/trunk@14151 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/data/DefinitionImport_Base.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.data;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.esjp.common.jasperreport.StandartReport;
+import org.efaps.esjp.common.parameter.ParameterUtil;
+import org.efaps.util.EFapsException;
+
+/**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_base</code>"
+ * class.
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("5b73a63b-7dd3-4af6-9a1c-d374a6e6c1d9")
+@EFapsRevision("$Rev$")
+public class DefinitionImport_Base
+{
+
+    public Return execute(final Parameter _parameter)
+        throws EFapsException
+    {
+        ParameterUtil.setProperty(_parameter, "JasperReport", "Data_ExportAttributeDefinitions");
+        ParameterUtil.setProperty(_parameter, "NoDataSource", "true");
+
+        new StandartReport().execute(_parameter);
+        return new Return();
+    }
+
+}
