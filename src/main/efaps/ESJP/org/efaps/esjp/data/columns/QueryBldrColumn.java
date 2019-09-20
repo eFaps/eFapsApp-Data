52fa455400a9c7c4648f90d76cd5396569393a2e
Jan Moxter <jan.moxter@moxter.net>
2019-09-20 17:00:17 -0500

    Changing to commons collection 4

--- src/main/efaps/ESJP/org/efaps/esjp/data/columns/QueryBldrColumn.java
+++ src/main/efaps/ESJP/org/efaps/esjp/data/columns/QueryBldrColumn.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2013 The eFaps Team
+ * Copyright 2003 - 2019 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Revision:        $Rev$
- * Last Changed:    $Date$
- * Last Changed By: $Author$
  */
 
 package org.efaps.esjp.data.columns;
@@ -26,7 +23,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.efaps.admin.datamodel.Type;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.program.esjp.EFapsApplication;
